5e45c2b50bf11ceded26d67c7b8f98a20aa19060
Sithvothy KIV <34917417+sithvothykiv@users.noreply.github.com>
2023-10-02 08:25:33 +0700

    add image background

--- game.py
+++ game.py
@@ -1,24 +1,36 @@
 from tkinter import *
 from tkinter import messagebox
-# I learn how to use tkinter from https://likegeeks.com/python-gui-examples-tkinter-tutorial/
-from random import random
 from PIL import Image, ImageTk
-# Download PIL to open image >>> open cmd >>> pip install pillow
-# I learn how to use PIL from https://python3.wannaphong.com/2014/11/image-processing-python.html
-
 from random import randint
 
 
-class Dragon_Tiger:
+class DragonTiger:
     def __init__(self):
         self.gui = Tk()
         self.__user_chips = 10000
         self.__passConfirm = FALSE
         self.__passChoose = FALSE
 
-        self.gui.geometry("1200x800")
+        self.gui.resizable(0, 0)
+
+        self.canvas = Canvas(self.gui, height=360, width=1333)
+        self.canvas.pack()
+
+        self.image = Image.open("img/background.png")
+        self.img_copy = self.image.copy()
+
+        self.background_image = ImageTk.PhotoImage(self.image)
+
+        # self.background = Label(self.gui, image=self.background_image)
+        # self.background.pack(fill=BOTH, expand=YES)
+        self.bg_label = self.canvas.create_image((0, 0), image=self.background_image, anchor=N + W)
+
+
+        # self.background.bind('<Configure>', self._resize_image)
+
+        self.gui.geometry("900x600")
         self.gui.title("Dragon Tiger")
-        self.gui.iconbitmap(r'img/cards1.ico')
+        # self.gui.iconbitmap(r'img/cards1.ico')
 
         self.__img = Image.open("img/back_card.png")
         self.__img = self.__img.resize((int(self.__img.width * .2), int(self.__img.height * .2)))
@@ -29,13 +41,15 @@ class Dragon_Tiger:
         self.__lbl.place(relx=0.3, rely=0.3, anchor=CENTER)
         self.__lbl2.place(relx=0.75, rely=0.3, anchor=CENTER)
 
-        bets = Label(text="BET  : ")
+        bets = Label(self.gui, text="BET: ", font=('', 45, 'bold'))
+        # bets = self.canvas.create_text((410, 120), text='BET: ', font=('', 45, 'bold'), fill="#652828")
         self.__input_bet = Entry(self.gui)
-        bets.place(relx=0.4, rely=0.8, anchor=CENTER)
-        self.__input_bet.place(relx=0.5, rely=0.8, anchor=CENTER)
+        bets.place(relx=0.35, rely=0.8, anchor=CENTER)
+        # self.canvas.itemconfig(bets)
+        self.__input_bet.place(relx=0.55, rely=0.8, anchor=CENTER)
 
-        # self.open = Button( self.gui, text="OPEN CARD", bg='black', command=self.open_card)
-        # self.open.place(relx = 0.1, rely = 0.6, anchor = CENTER)
+        self.open = Button(self.gui, text="OPEN CARD", bg='black', command=self.open_card)
+        self.open.place(relx=0.1, rely=0.6, anchor=CENTER)
         self.dragon = Button(self.gui, text="DRAGON", bg='red', command=self.choose_dragon)
         self.dragon.place(relx=0.3, rely=0.6, anchor=CENTER)
         self.tiger = Button(self.gui, text="TIGER", bg='blue', command=self.choose_tiger)
@@ -60,20 +74,20 @@ class Dragon_Tiger:
         self.__user_chips = user_chips
 
     @property
-    def passConfirm(self):
+    def pass_confirm(self):
         return self.__passConfirm
 
-    @passConfirm.setter
-    def passConfirm(self, passConfirm):
-        self.__passConfirm = passConfirm
+    @pass_confirm.setter
+    def pass_confirm(self, pass_confirm):
+        self.__passConfirm = pass_confirm
 
     @property
-    def passChoose(self):
+    def pass_choose(self):
         return self.__passChoose
 
-    @passChoose.setter
-    def passChoose(self, passChoose):
-        self.__passChoose = passChoose
+    @pass_choose.setter
+    def pass_choose(self, pass_choose):
+        self.__passChoose = pass_choose
 
     @property
     def img(self):
@@ -144,20 +158,20 @@ class Dragon_Tiger:
 
     def confirm(self):
         self.__val = self.__input_bet.get()
-        if (not (self.__val.isdigit())):
+        if not (self.__val.isdigit()):
             messagebox.showerror("Error", "Not Integer Number")
-        elif (not (self.__choose == 1 or self.__choose == 2 or self.__choose == 0)):
+        elif not (self.__choose == 1 or self.__choose == 2 or self.__choose == 0):
             messagebox.showerror("Error", "Please Choose dragon OR tiger OR draw")
         else:
             try:
-                if (int(self.__val) > self.__user_chips or int(self.__val) <= 0):
+                if int(self.__val) > self.__user_chips or int(self.__val) <= 0:
                     messagebox.showerror("Sorry", "You have lost all your chips.")
                 else:
 
                     messagebox.showinfo("Success", "Bet Successful")
-                    # self.open_card(self)
-                    # self.__passConfirm=TRUE
-                    if (self.__passChoose == TRUE):
+                    self.open_card(self)
+                    self.__passConfirm = TRUE
+                    if self.__passChoose == TRUE:
                         Dragon = randint(1, 13)
                         Tiger = randint(1, 13)
 
@@ -210,55 +224,65 @@ class Dragon_Tiger:
             except ValueError:
                 messagebox.showerror("Error", "Error message")
 
-    # def open_card(self):
-    #     if(self.__passConfirm == TRUE and self.__passChoose == TRUE):
-    #         Dragon = randint(1,13)
-    #         Tiger = randint(1,13)
+    def open_card(self):
+        if (self.__passConfirm == TRUE and self.__passChoose == TRUE):
+            Dragon = randint(1, 13)
+            Tiger = randint(1, 13)
 
-    #         opDragon = randint(1,4)
-    #         opTiger = randint(1,4)
+            opDragon = randint(1, 4)
+            opTiger = randint(1, 4)
 
-    #         self.imgDra = Image.open("img/cards/"+str(Dragon)+"_"+str(opDragon)+".PNG")
-    #         self.photoDra = ImageTk.PhotoImage(self.imgDra)
-    #         self.imgTi = Image.open("img/cards/"+str(Tiger)+"_"+str(opTiger)+".PNG")
-    #         self.photoTi = ImageTk.PhotoImage(self.imgTi)
-    #         self.__lbl = Label(image=self.photoDra)
-    #         self.__lbl1 = Label(image=self.photoTi)
-    #         self.__lbl.place(relx=0.3, rely=0.3, anchor=CENTER)
-    #         self.__lbl1.place(relx=0.75, rely=0.3, anchor=CENTER)
+            self.imgDra = Image.open("img/cards/" + str(Dragon) + "_" + str(opDragon) + ".PNG")
+            self.photoDra = ImageTk.PhotoImage(self.imgDra)
+            self.imgTi = Image.open("img/cards/" + str(Tiger) + "_" + str(opTiger) + ".PNG")
+            self.photoTi = ImageTk.PhotoImage(self.imgTi)
+            self.__lbl = Label(image=self.photoDra)
+            self.__lbl1 = Label(image=self.photoTi)
+            self.__lbl.place(relx=0.3, rely=0.3, anchor=CENTER)
+            self.__lbl1.place(relx=0.75, rely=0.3, anchor=CENTER)
 
-    #         win = 99
-    #         if(Dragon<Tiger):
-    #             win = 1
-    #         elif (Dragon>Tiger):
-    #             win = 0
-    #         elif (Dragon==Tiger):
-    #             win = 2
+            win = 99
+            if (Dragon < Tiger):
+                win = 1
+            elif (Dragon > Tiger):
+                win = 0
+            elif (Dragon == Tiger):
+                win = 2
 
-    #         if (self.__choose==win):
-    #             if(win!=2):
-    #                 self.__val = int(self.__val) * 1
-    #             else:
-    #                 self.__val = int(self.__val) * 7
-    #             self.__user_chips = self.__user_chips + int(self.__val)
-    #             messagebox.showinfo("You Win", " + "+str(self.__val)+" Chips")
-    #         else:
-    #             if (win != 2):
-    #                 self.__user_chips = self.__user_chips - int(self.__val)
-    #                 messagebox.showinfo("You Lose", " - " + str(self.__val) + " Chips")
-    #             else:
-    #                 self.__user_chips = self.__user_chips - (int(self.__val)/2)
-    #                 messagebox.showinfo("You Lose", " - " + str((int(self.__val)/2)) + " Chips")
+            if (self.__choose == win):
+                if (win != 2):
+                    self.__val = int(self.__val) * 1
+                else:
+                    self.__val = int(self.__val) * 7
+                self.__user_chips = self.__user_chips + int(self.__val)
+                messagebox.showinfo("You Win", " + " + str(self.__val) + " Chips")
+            else:
+                if (win != 2):
+                    self.__user_chips = self.__user_chips - int(self.__val)
+                    messagebox.showinfo("You Lose", " - " + str(self.__val) + " Chips")
+                else:
+                    self.__user_chips = self.__user_chips - (int(self.__val) / 2)
+                    messagebox.showinfo("You Lose", " - " + str((int(self.__val) / 2)) + " Chips")
 
-    #         self.__chip1 = Label(text=f"Chips   :  {self.__user_chips}  ")
-    #         self.__chip1.place(relx=0.05, rely=0.05, anchor=CENTER)
-    #         self.__passChoose = FALSE
-    #         self.__passConfirm = FALSE
-    #         self.__choose = 99
-    #         self.display()
+            self.__chip1 = Label(text=f"Chips   :  {self.__user_chips}  ")
+            self.__chip1.place(relx=0.05, rely=0.05, anchor=CENTER)
+            self.__passChoose = FALSE
+            self.__passConfirm = FALSE
+            self.__choose = 99
+            self.display()
 
-    #     else:
-    #         messagebox.showerror("Error", "Betting  Unsuccessful")
+        else:
+            messagebox.showerror("Error", "Betting  Unsuccessful")
+
+    def _resize_image(self, event):
+
+        new_width = event.width
+        new_height = event.height
+
+        self.image = self.img_copy.resize((new_width, new_height))
+
+        self.background_image = ImageTk.PhotoImage(self.image)
+        self.background.configure(image=self.background_image)
 
     def exit(self):
         self.gui.destroy()
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from game import *
 
-Dragon_Tiger()
+DragonTiger()
